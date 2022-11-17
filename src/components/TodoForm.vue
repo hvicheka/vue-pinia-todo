e6fcf05bad635bc07dc8d48e5827eea809393eb1
Vichaka <h-vichaka@beniten.com>
2022-11-17 09:17:52 +0700

    start

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <form @submit.prevent="addItemAndClear(todo)">
+            <input v-model="todo" type="text" /><button>Add</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useTodoListStore } from '@/store/useTodoListStore'
+
+export default {
+    
+    setup () {
+        const todo = ref("");
+        const store = useTodoListStore()
+
+        function addItemAndClear(item) {
+            if (!item) {
+                return;
+            }
+            store.addTodo(item);
+            todo.value = "";
+        }
+        return { todo, addItemAndClear, store };
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-for="todo in todoList" :key="todo.id">
+            <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+            <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+            <span @click="deleteTodo(todo.id)">&#10060;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { useTodoListStore } from '@/store/useTodoListStore'
+import { storeToRefs } from "pinia";
+
+export default {
+    setup () {
+        const store = useTodoListStore();
+        const { todoList } = storeToRefs(store);
+        const { toggleCompleted,deleteTodo  } = store;
+        return {todoList,toggleCompleted,deleteTodo  }
+    }
+}
+</script>
+
+<style scoped>
+    .completed {
+    text-decoration: line-through;
+    }
+</style>
